0a13922acab7a52ec72adb56837ce0e746cee79b
juanjo2702 <juanjo270201@gmail.com>
2026-02-05 16:30:52 -0400

    chore: remove temporary debug script

--- check_column.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "Checking 'grupos' table schema...\n";
-$exists = Schema::hasColumn('grupos', 'carrera_id');
-if ($exists) {
-    echo "Column 'carrera_id' EXISTS.\n";
-} else {
-    echo "Column 'carrera_id' MISSING.\n";
-}
