76708f191a42e6c29d79fd2aa65ab504ac32cc5e
juanjo2702 <juanjo270201@gmail.com>
2026-02-06 02:02:07 -0400

    chore: retry include sigla in debug dump

--- debug_carreras.php
+++ debug_carreras.php
@@ -8,6 +8,6 @@ $kernel->bootstrap();
 
 use App\Models\Carrera;
 
-$carreras = Carrera::all(['id', 'nombre', 'codigo']);
+$carreras = Carrera::all(['id', 'nombre', 'codigo', 'sigla']);
 file_put_contents('carreras_dump.json', json_encode($carreras, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 echo "Dumped " . $carreras->count() . " careers to carreras_dump.json\n";
