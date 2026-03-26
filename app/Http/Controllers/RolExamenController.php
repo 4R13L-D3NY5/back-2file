b3ea9d673882781a43b9da6638d18e5d1d96267c
4R13L-D3NY5 <4r13l.d3ny5@gmail.com>
2026-03-26 10:22:50 -0400

    revisar mostrar columna de preguntas en gestion de evaluaciones
    revisar mostrar columna de preguntas en gestion de evaluaciones

--- app/Http/Controllers/RolExamenController.php
+++ app/Http/Controllers/RolExamenController.php
@@ -25,7 +25,13 @@ class RolExamenController extends Controller
                 \DB::raw('COALESCE(MAX(grupos.asignatura_id), MAX(asignaturas.id)) as asignatura_id'),
                 \DB::raw('MAX(docentes.id) as docente_id'),
                 \DB::raw('MAX(docentes.nombre_completo) as docente'),
-                \DB::raw('MAX(asignatura_carrera.semestre) as semestre')
+                \DB::raw('MAX(asignatura_carrera.semestre) as semestre'),
+                \DB::raw("(SELECT COUNT(*) FROM banco_preguntas 
+                           WHERE banco_preguntas.asignatura_id = asignaturas.id 
+                           AND banco_preguntas.docente_id = docentes.id
+                           AND banco_preguntas.parcial = rol_examenes.tipo_examen 
+                           AND (banco_preguntas.grupoTeorico = rol_examenes.grupo OR banco_preguntas.grupoTeorico LIKE CONCAT('%', rol_examenes.grupo, '%'))
+                          ) as total_banco")
             )
             ->join('asignaturas', 'rol_examenes.materia_codigo', '=', 'asignaturas.codigo')
             ->join('carreras', 'rol_examenes.carrera_id', '=', 'carreras.id')
